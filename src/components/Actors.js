4b23e90db45de8572dd674ad66837145e5abebe7
Melissa Vaiden <melissavaiden@gmail.com>
2022-07-26 20:58:55 -0400

    Completed Assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,28 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actor = actors.map((actor) => {
+    return (
+      <div key={actor.name}>
+        {actor.name}
+        <ul>
+          {actor.movies.map((movie) => {
+            return (
+              <li key={movie}>{movie}</li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  })
+
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actor}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+ const movie = movies.map((movie) => {
+      return (
+        <div key={movie.title}>
+          {movie.title}
+          {movie.time}
+          <ul>{movie.genres.map((genre) => {
+              return <li key={genre}>{genre}</li>
+            })}
+          </ul>
+        </div>
+      )
+    })
+  return <div>
+    <h1>Movies Page</h1>
+    <div>{movie}</div>
+  </div>;
 }
 
 export default Movies;
